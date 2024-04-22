3b12af3861211002280d81ab3863bed0ba57221d
iren42 <isabelleren618@gmail.com>
2024-04-22 10:07:53 +0200

    up: translation from html to js for the navbar and the chat

---
+++ Transcendance_Back/Transcendance/templates/transcendance/chat.listener.js
@@ -0,0 +1,66 @@
+const btn_nav = document.querySelectorAll('button.btn-nav');
+// const arrow_tab = document.querySelector("button.btn-arrow");
+
+const discussions = document.querySelectorAll('#disc_pane > button.btn-heart-list');
+const users = document.querySelectorAll('#user_pane > button.btn-heart-list');
+
+// const action_pane = document.querySelectorAll("#action_pane");
+// const chatroom = document.getElementById("chatroom-tab");
+// const chat_list = document.querySelectorAll("div.chat__list");
+let current_pane = document.querySelector('.tab-pane.active.show');
+
+function get_active_pane() {
+    return (document.querySelector('.tab-pane.active.show'));
+}
+
+
+// listener for navbar buttons
+btn_nav.forEach(triggerEl => {
+    const tabTrigger = new bootstrap.Tab(triggerEl);
+
+    triggerEl.addEventListener('click', event => {
+        event.preventDefault();
+        tabTrigger.show();
+    })
+})
+
+arrow_tab.addEventListener("click", event => {
+    event.preventDefault();
+    arrow_tab.classList.add("hide");
+    chatroom.style.display = "none";
+    action_pane.style.display = "none";
+    btn_nav.forEach(triggerE => {
+        triggerE.classList.remove("hide");
+    })
+
+    current_pane.classList.add("active", "show");
+})
+
+// listener for list items
+discussions.forEach(element => {
+    element.addEventListener('click', event => {
+        event.preventDefault();
+        btn_nav.forEach(triggerE => {
+            triggerE.classList.add("hide");
+        })
+        current_pane = get_active_pane();
+        current_pane.classList.remove("active", "show");
+        arrow_tab.classList.remove("hide");
+
+        chatroom.style.display = "flex";
+    })
+})
+
+users.forEach(element => {
+    element.addEventListener('click', event => {
+        event.preventDefault();
+        btn_nav.forEach(triggerE => {
+            triggerE.classList.add("hide");
+        })
+        current_pane = get_active_pane();
+        current_pane.classList.remove("active", "show");
+        arrow_tab.classList.remove("hide");
+
+        action_pane.style.display = "flex";
+    })
+})
