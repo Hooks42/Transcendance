4b3decc4c4819075f6174d6e973eda7869b4882e
hooks42 <atgie.cedric2@gmail.com>
2024-05-03 22:21:17 +0200

    derniere version avec nouvel env (django_env)

--- Transcendance_Back/Transcendance_Back/settings.py
+++ Transcendance_Back/Transcendance_Back/settings.py
@@ -144,7 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles/Transcendance",
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
