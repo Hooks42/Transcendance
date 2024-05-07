ae7a898baed21c421933a40da5f2998ed99f0840
hooks42 <atgie.cedric2@gmail.com>
2024-05-07 09:38:02 +0200

    modification de la creation et du lancement du socket de chat a un objet socket special pour lui ✅

--- Transcendance_Back/staticfiles/Transcendance/js/chat.js
+++ Transcendance_Back/staticfiles/Transcendance/js/chat.js
@@ -230,31 +230,4 @@ const chat = {
     {
         return (document.querySelector('.tab-pane.active.show'));
     },
-
-    launch_socket: function ()
-    {
-        this.chat_socket = new WebSocket('wss://localhost/ws/chat/');
-
-        this.chat_socket.onopen = function (e)
-        {
-            console.log('Socket opened');
-            chat.create();
-            chat.load();
-            navbar.create();
-            navbar.load();
-        };
-
-        this.chat_socket.onclose = function (e)
-        {
-            console.error('Socket closed');
-        }
-
-        this.chat_socket.onmessage = (event) => 
-        {
-            let data = JSON.parse(event.data);
-            console.log("message recieved ---> " + data.message);
-            console.log("profile_picture ---> " + data.profile_picture);
-            this.add_chat(data.username, data.timestamp, data.message, data.profile_picture);
-        };
-    }
 }
--- Transcendance_Back/staticfiles/Transcendance/js/clear_connexion_page.js
+++ Transcendance_Back/staticfiles/Transcendance/js/clear_connexion_page.js
@@ -2,5 +2,5 @@ function clear_connexion_page()
 {
 	let log_div = document.getElementById('log-div');
 	log_div.remove();
-	chat.launch_socket();
+	socket.launch_socket();
 }
